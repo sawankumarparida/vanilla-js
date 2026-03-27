6979dae218c9ed3925f4d69c00a63edc1811cb3b
SAWAN KUMAR PARIDA <139310923+sawankumarparida@users.noreply.github.com>
2026-03-27 18:02:50 +0530

    python livereload

---
+++ serve.py
@@ -0,0 +1,8 @@
+from livereload import Server
+server = Server()
+# Watch your HTML, JS, and CSS files
+server.watch('*.html')
+server.watch('*.js')
+server.watch('*.css')
+# Start the server
+server.serve(root='.', port=5500)
