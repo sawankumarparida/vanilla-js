4057cd151c8aaf3631335a78d1f47fdabadfd086
SAWAN KUMAR PARIDA <139310923+sawankumarparida@users.noreply.github.com>
2026-04-06 13:45:28 +0530

    alignment change

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ btn.addEventListener('click', () => {
     // Change the color so we know it worked!
     display.style.color = "green";
 });
+
+const toggleBtn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+// 1. Check for saved theme on load
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleBtn.textContent = "Switch to Light Mode";
+    }
+}
+
+// 2. Add click event
+toggleBtn.addEventListener('click', () => {
+    let theme = document.documentElement.getAttribute('data-theme');
+    
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        toggleBtn.textContent = "Switch to Dark Mode";
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        toggleBtn.textContent = "Switch to Light Mode";
+        localStorage.setItem('theme', 'dark');
+    }
+});
